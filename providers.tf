bbf7e92c4a9e97cbc4c3b4ac9a80c4dde9d28204
megha-kaushik <megha.kaushik@rackspace.com>
2023-07-13 10:46:35 +0530

    removing credentials

--- providers.tf
+++ providers.tf
@@ -17,7 +17,6 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
 #   profile = "arn:aws:iam::<your account>:instance-profile/<your role name>" 
-  # access_key = "AKIAYRJBGX2UBDW622XO"
-  # secret_key = "Bj7k3v9sDX2zjaQ2oQekx0AO1ZZKn+u8ixYHq0pU"
+  
   
 }
